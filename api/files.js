6161a0709a9a6f3bf7eade38ca863438c6b7baa0
FDMZ17 <devoloper.fdmz17@gmail.com>
2022-06-26 08:54:55 +0700

    Sun Jun 26 08:54:55 AM WIB 2022 File list

--- api/files.js
+++ api/files.js
@@ -1,7 +1,16 @@
+const config = require("../config.js");
+
 module.exports.load = async function(app, db) {
   app.get("/files", async (req, res) => {
     if (req.session.loggedIn) {
-      let fileList = await db.get(`${req.session.name}.files`);
+      let dbList = await db.get(`${req.session.name}.files`);
+      let i = 0;
+      let listFile = [];
+      while (i < dbList.length) {
+        listFile.push(`<li> <a class="inline-block text-base hover:text-primary mb-3" href="${config.WEB_URL}/${dbList[i]}">${dbList[i]}</a> </li>`);
+        i++;
+      }
+
       res.send(`
 <!DOCTYPE html>
 <html lang="en">
@@ -18,21 +27,10 @@ module.exports.load = async function(app, db) {
 	<div class="mt-16 max-w-2xl p-5 mx-auto rounded shadow-sm">
     <h2 class="px-4 text-4xl text-white text-center">Simple cloud dashboard</h2>
     <p class="px-4 text-xl text-white text-center mt-12">Welcome back ${req.session.name}</p>
-    <p class="px-4 text-lg text-white items-center mt-5">Your upoaded fileID: ${fileList}</p>
     <div class="w-full">
-      <h3 class="font-semibold text-xl text-white mb-3">Your file:</h3>
-      <ul class="text-slate-300">
-      <li>
-      <a class="inline-block text-base hover:text-primary mb-3" href="#">File1</a>
-      </li>
-      <a class="inline-block text-base hover:text-primary mb-3" href="#">File2</a>
-      <li>
-      <a class="inline-block text-base hover:text-primary mb-3" href="#">File3</a>
-      </li>
-      <a class="inline-block text-base hover:text-primary mb-3" href="#">File4</a>
-      <li>
-      <a class="inline-block text-base hover:text-primary mb-3" href="#">File5</a>
-      </li>
+      <h3 class="font-semibold text-xl text-white mb-3 text-center mt-8">Your file:</h3>
+      <ul class="text-slate-300 text-center">
+      ${listFile.join("")}
       </ul>
     </div>
   </div>
