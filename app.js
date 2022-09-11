a97a86574fb3d4ee566266d1489c2e9d40cb0233
FDMZ17 <developer@fdmz17.eu.org>
2022-09-11 14:28:42 +0700

    Auto update!

--- app.js
+++ app.js
@@ -56,9 +56,11 @@ if (!fs.existsSync("usercontent")) {
   console.log("Creating usercontent directory");
 }
 
-setInterval(function A() {
-  require('child_process').exec('git pull')
-}, 600000);
+if (config.server.autoUpdate) {
+  setInterval(function A() {
+    require('child_process').exec('git pull')
+  }, 600000);
+}
 
 app.listen(config.website.port);
 console.log("Running on: " + config.website.app_url);
